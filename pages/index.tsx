ce83b87a7bb9a9370ba777cbf8eb160137b664b9
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-12 19:18:26 -0300

    chore: create project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>KGT</title>
+        <meta name="description" content="DAPP" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>KGT Hello World</p>
+      </main>
+    </div>
+  );
+}
